cfdd1c869f85818e3d8357eef22687df9c0b5619
Alejandro <alejandroaperez1994g@gmail.com>
2023-08-29 16:20:33 +0200

    :sparkles: added health endpoint in express app

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,17 @@
-// index.ts
 import express from 'express';
 import {router as projectRoutes} from './adapters/express/routes/projectRoutes';
 import {skillRouter} from "./adapters/express/routes/skillRoutes";
-import {journeyRoutes} from "./adapters/express/routes/journeyRoutes"; // Ajusta la ubicación de tus rutas
+import {journeyRoutes} from "./adapters/express/routes/journeyRoutes";
+import {healthRouter} from "./health/healthRouter";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Conecta a la base de datos MongoDB utilizando Prisma
 
-
-// Middleware para permitir el análisis de solicitudes JSON
 app.use(express.json());
 
-// Rutas para proyectos (ajusta la ruta según tus necesidades)
+app.use("/health", healthRouter)
+
 app.use('/api/projects', projectRoutes);
 app.use("/api/skills", skillRouter)
 app.use("/api/journeys", journeyRoutes)
